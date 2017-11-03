a4f7f4a3578ec0fa0eb05417799b3a92f91ef330
Bassam Kurdali <bassam@urchn.org>
2017-11-03 13:36:02 -0400

    removed unneeded file

--- kognito_rig_tools/driver.py
+++
@@ -1,9 +0,0 @@
-import bpy
-from mathutils import Vector
-
-cached_value = Vector((0.0, 0.028712928295135498, -0.7043251991271973))
-
-for i in range(3):
-    def scaler(scale):
-        return ((scale - 1) * cached_value)[i]
-    bpy.app.driver_namespace['scale_{}'.format(i)] = scaler
